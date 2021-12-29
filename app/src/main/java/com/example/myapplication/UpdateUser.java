93aa8350bfb14076de1801714697b9ea1f60b2be
עדי מור <206556755@idf.il>
2021-12-29 16:50:30 +0200

    update user

--- app/src/main/java/com/example/myapplication/UpdateUser.java
+++ app/src/main/java/com/example/myapplication/UpdateUser.java
@@ -7,18 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
 public class UpdateUser extends AppCompatActivity {
 
     private Button updateUser;
+    private EditText description;
+    private EditText seniority;
+    private EditText profession;
+
     DatabaseReference databaseReference;
 
     @Override
@@ -27,32 +36,42 @@ public class UpdateUser extends AppCompatActivity {
         setContentView(R.layout.activity_update_user);
 
         updateUser = findViewById(R.id.updateUser);
+        description = findViewById(R.id.description);
+        seniority = findViewById(R.id.seniority);
+        profession = findViewById(R.id.profession);
 
         updateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateUserData(LoginUser.getLoginEmail(), "aaaaaa");
+                String txt_description = description.getText().toString();
+                String txt_seniority = seniority.getText().toString();
+                String txt_profession = profession.getText().toString();
+
+                updateUserData(txt_description, txt_seniority, txt_profession);
             }
         });
     }
 
-    private void updateUserData(String email, String desctiption) {
+    private void updateUserData(String description, String seniority, String profession) {
         HashMap User = new HashMap();
-        User.put("email", 2222);
-        User.put("name", 2222);
-        User.put("description", 22222);
+        User.put("description", description);
+        User.put("seniority", seniority);
+        User.put("profession", profession);
+
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
-        databaseReference.child("MrmW0U0eOPPjQda_4np").updateChildren(User).addOnCompleteListener(new OnCompleteListener() {
+        databaseReference.orderByChild("email").equalTo(LoginUser.getLoginEmail()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(UpdateUser.this, "Successfully Updated", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(UpdateUser.this, "Failed to Update", Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot childSnapshot: snapshot.getChildren()){
+                    String key = childSnapshot.getKey();
+                    databaseReference.child(key).updateChildren(User);
                 }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
-
     }
 }
 
