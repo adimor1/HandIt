d5012d2efd1ce711d875d7cd27d06855ebb40f68
עדי מור <206556755@idf.il>
2021-12-30 16:47:32 +0200

    add dialog

--- app/src/main/java/com/example/myapplication/ProfessionalDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/ProfessionalDetailsActivity.java
@@ -2,10 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,16 +22,19 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
 
     private FirebaseDatabase db;
     private DatabaseReference reference;
-    private Button order;
-    
+    private Button orderBtn;
+    String emailFromTv;
+    TextView tvEmail;
+    TextView infoTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        order = findViewById(R.id.order);
-        setContentView(R.layout.activity_professional_details);
 
-        TextView tvEmail = (TextView) findViewById(R.id.tvEmailDetails);
+        setContentView(R.layout.activity_professional_details);
+        orderBtn = findViewById(R.id.orderBtn);
+        tvEmail = (TextView) findViewById(R.id.tvEmailDetails);
         TextView tvDescription = (TextView) findViewById(R.id.tvDescriptionDetails);
 
         if(getIntent().hasExtra("selected_user")){
@@ -39,21 +46,61 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
             tvDescription.setText(description);
         }
 
-        order.setOnClickListener(new View.OnClickListener() {
+        orderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
-             //   Toast.makeText(ProfessionalDetailsActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
-           //     addOrder(emailPro);
+                emailFromTv = tvEmail.getText().toString();
+                addOrder(emailFromTv);
+                showCustomDialog();
             }
         });
     }
 
-  //  private void addOrder(String emailPro){
-    //    Order order= new Order(emailPro, LoginUser.getLoginEmail());
-      //  db= FirebaseDatabase.getInstance();
-        //String key = db.getReference("Orders").push().getKey();
-        //reference= db.getReference("Orders");
-        //reference.child(key).setValue(order);
-    //}
+    private void addOrder(String emailPro){
+        Order order= new Order(emailPro, LoginUser.getLoginEmail());
+        db= FirebaseDatabase.getInstance();
+        String key = db.getReference("Orders").push().getKey();
+        reference= db.getReference("Orders");
+        reference.child(key).setValue(order);
+    }
+
+    void showCustomDialog() {
+        final Dialog dialog = new Dialog(ProfessionalDetailsActivity.this);
+        //We have added a title in the custom layout. So let's disable the default title.
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //The user will be able to cancel the dialog bu clicking anywhere outside the dialog.
+        dialog.setCancelable(true);
+        //Mention the name of the layout of your custom dialog.
+        dialog.setContentView(R.layout.custom_dialog);
+
+        //Initializing the views of the dialog.
+        final EditText nameEt = dialog.findViewById(R.id.name_et);
+        final EditText ageEt = dialog.findViewById(R.id.age_et);
+        final CheckBox termsCb = dialog.findViewById(R.id.terms_cb);
+        Button submitButton = dialog.findViewById(R.id.submit_button);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEt.getText().toString();
+                String age = ageEt.getText().toString();
+                Boolean hasAccepted = termsCb.isChecked();
+                populateInfoTv(name,age,hasAccepted);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    void populateInfoTv(String name, String age, Boolean hasAcceptedTerms) {
+        infoTv.setVisibility(View.VISIBLE);
+        String acceptedText = "have";
+        if(!hasAcceptedTerms) {
+            acceptedText = "have not";
+        }
+       // infoTv.setText(String.format(getString(R.string.info), name, age, acceptedText));
+    }
 }
--- app/src/main/java/com/example/myapplication/ProfessionalsListActivity.java
+++ app/src/main/java/com/example/myapplication/ProfessionalsListActivity.java
@@ -7,13 +7,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -23,6 +29,8 @@ public class ProfessionalsListActivity extends AppCompatActivity implements MyAd
     DatabaseReference database;
     MyAdapter myAdapter;
     ArrayList<User> list;
+    StorageReference storageReference;
+    private ImageView imageProf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,10 @@ public class ProfessionalsListActivity extends AppCompatActivity implements MyAd
         myAdapter = new MyAdapter(this, list, this);
         recyclerView.setAdapter(myAdapter);
 
+        imageProf = findViewById(R.id.imageProf);
+
+
+
         database.addValueEventListener(new ValueEventListener() {
             @SuppressLint("NotifyDataSetChanged")
             @Override
@@ -46,6 +58,16 @@ public class ProfessionalsListActivity extends AppCompatActivity implements MyAd
 
                     User user = dataSnapshot.getValue(User.class);
                     list.add(user);
+
+                    storageReference = FirebaseStorage.getInstance().getReference();
+
+                    //StorageReference profileRef = storageReference.child("users/"+"adimor@gmail.com"+"/profile.jpg");
+                    //profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                      //  @Override
+                        //public void onSuccess(Uri uri) {
+                         //   Picasso.get().load(uri).into(imageProf);
+                        //}
+                    //});
                 }
                 myAdapter.notifyDataSetChanged();
             }
