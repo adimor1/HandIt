03ba41802a2359e0fc02deb35e08f2d5d0bc3a76
עדי מור <206556755@idf.il>
2022-01-26 19:14:12 +0200

    finish order with date & time picker

--- app/src/main/java/com/example/myapplication/Activities/ProfessionalDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/ProfessionalDetailsActivity.java
@@ -3,17 +3,24 @@ package com.example.myapplication.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.myapplication.Models.LoginUser;
@@ -31,7 +38,9 @@ import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class ProfessionalDetailsActivity extends AppCompatActivity {
 
@@ -50,6 +59,11 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
     private ImageView imageDetails;
     private RatingBar rBar;
     private Button btn;
+    Button timeButton;
+    int hour;
+    int minute;
+    private DatePickerDialog datePickerDialog;
+    Button dateButton;
 
     DatabaseReference databaseReference;
     StorageReference storageReference;
@@ -156,24 +170,33 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(true);
         dialog.setContentView(R.layout.custom_dialog);
-        final EditText timeEt = dialog.findViewById(R.id.timeEt);
-        final EditText dateEt = dialog.findViewById(R.id.dateEt);
-
-   
-
         Button submitButton = dialog.findViewById(R.id.submit_button);
+        timeButton = dialog.findViewById(R.id.timeButton);
+        initDatePicker();
+        dateButton=dialog.findViewById(R.id.dateButton);
+        dateButton.setText(getTodayDate());
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String time = timeEt.getText().toString();
-                String date = dateEt.getText().toString();
+                String date = dateButton.getText().toString();
+                String time = timeButton.getText().toString();
                 addOrder(emailFromTv, time, date);
                 dialog.dismiss();
             }
         });
         dialog.show();
     }
+
+    private String getTodayDate(){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month+1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
 
     private void addOrder(String emailPro, String time, String date){
         Order order= new Order(emailPro, LoginUser.getLoginEmail(), time, date);
@@ -186,5 +209,88 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
     private double finalRatingCal(double finalRating){
         return Math.ceil(finalRating * 2) / 2;
 
+    }
+
+    public void popTimePicker(View view){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener=new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                hour = hourOfDay;
+                minute = minute;
+                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("select time");
+        timePickerDialog.show();
+    }
+
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener= new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month +1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+         datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year){
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month){
+        if(month==1){
+            return "JAN";
+        }
+        if(month==2){
+            return "FEB";
+        }
+        if(month==3){
+            return "MAR";
+        }
+        if(month==4){
+            return "APR";
+        }
+        if(month==5){
+            return "MAY";
+        }
+        if(month==6){
+            return "JUN";
+        }
+        if(month==7){
+            return "JUL";
+        }
+        if(month==8){
+            return "AUG";
+        }
+        if(month==9){
+            return "SEP";
+        }
+        if(month==10){
+            return "OCT";
+        }
+        if(month==11){
+            return "NOV";
+        }
+        if(month==12){
+            return "DEC";
+        }
+        return "JAN";
+    }
+
+    public void popDatePicker(View view){
+        datePickerDialog.show();
     }
 }
